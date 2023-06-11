7a1beba5fbba8984dcc208ee6113f556a5fcc40c
Leonardo Axel Setyanto <laxels@gmail.com>
2023-06-11 13:10:20 -0700

    composite image

--- src/lib/components/CompositeImage.tsx
+++ src/lib/components/CompositeImage.tsx
@@ -1,4 +1,11 @@
-import { DetailedHTMLProps, ImgHTMLAttributes, memo, useState } from "react";
+import {
+  DetailedHTMLProps,
+  ImgHTMLAttributes,
+  memo,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { FC } from "../types";
 
 type CompositeImageProps = {
@@ -13,7 +20,11 @@ const CompositeImageComp: FC<CompositeImageProps> = ({
   const [sliderValue, setSliderValue] = useState(50);
   return (
     <Container>
-      <Image src={qrCodeDataURL} />
+      <Composite
+        imgSrcLeft={qrCodeDataURL}
+        imgSrcRight={imgSrc}
+        position={sliderValue}
+      />
       <Slider value={sliderValue} onChange={setSliderValue} />
     </Container>
   );
@@ -21,8 +32,74 @@ const CompositeImageComp: FC<CompositeImageProps> = ({
 
 export const CompositeImage = memo(CompositeImageComp);
 
+type CompositeProps = {
+  imgSrcLeft: string;
+  imgSrcRight: string;
+  position: number;
+};
+
+const CompositeComp: FC<CompositeProps> = ({
+  imgSrcLeft,
+  imgSrcRight,
+  position,
+}) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [containerWidth, setContainerWidth] = useState<number | null>(null);
+
+  useLayoutEffect(() => {
+    const containerEl = containerRef.current;
+    if (containerEl == null) {
+      return;
+    }
+    setContainerWidth(containerEl.clientWidth);
+  }, []);
+
+  return (
+    <div className="flex" ref={containerRef}>
+      {containerWidth != null && (
+        <>
+          <ImageContainer widthPercentage={position}>
+            <Image style={{ width: containerWidth }} src={imgSrcLeft} />
+          </ImageContainer>
+          <ImageContainer widthPercentage={100 - position}>
+            <Image
+              style={{
+                width: containerWidth,
+                transform: `translateX(-${position}%)`,
+              }}
+              src={imgSrcRight}
+            />
+          </ImageContainer>
+        </>
+      )}
+    </div>
+  );
+};
+
+const Composite = memo(CompositeComp);
+
 const Container: FC = ({ children }) => (
   <div className="w-full max-w-lg">{children}</div>
+);
+
+const CompositeContainer: FC = ({ children }) => (
+  <div className="relative flex">{children}</div>
+);
+
+type ImageContainerProps = {
+  widthPercentage: number;
+};
+
+const ImageContainer: FC<ImageContainerProps> = ({
+  widthPercentage,
+  children,
+}) => (
+  <div
+    className={`overflow-hidden shrink-0`}
+    style={{ width: `${widthPercentage}%` }}
+  >
+    {children}
+  </div>
 );
 
 type SliderProps = {
@@ -41,4 +118,6 @@ const Slider: FC<SliderProps> = ({ value, onChange }) => (
 
 const Image: FC<
   DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>
-> = ({ ...imgProps }) => <img className="w-full" {...imgProps} />;
+> = ({ ...imgProps }) => (
+  <img className="max-w-none aspect-square" {...imgProps} />
+);
