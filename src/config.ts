fb6bf4730ea59009d9705d5bf235b6bbb3293954
Charles Frye <cfrye59@gmail.com>
2023-06-12 00:56:36 -0700

    make em wait, make em laugh

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,15 @@ export const BACKEND_URL = `https://charlesfrye--qart-api.modal.run`;
 export const PLACEHOLDER_IMAGE_URL = `https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/okami-amaterasu-two-milford-martin.jpg`;
 
 export const LOADING_MESSAGES = [
-  `Generating image...`,
   `Compositing QR code...`,
+  `Generating image...`,
+  `Diffusing (but stably)...`,
+  `Cold booting (brr)...`,
+  `Evading safety filters...`,
   `Reticulating splines...`,
+  `Denoising latents...`,
+  `Competing for attention...`,
+  `Literally just vibing...`,
 ];
 
 export const LOADING_MESSAGE_INTERVAL_MS = 2000;
