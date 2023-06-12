4319f5225a7790178f95bf54ce8514e77dafda61
Charles Frye <cfrye59@gmail.com>
2023-06-12 01:48:18 -0700

    actually querying the backend

--- src/lib/qrcode.ts
+++ src/lib/qrcode.ts
@@ -13,12 +13,39 @@ export async function generateQRCodeDataURL(
   return null;
 }
 
-export async function generateImage(): Promise<string | null> {
+export async function generateImage(prompt: string, dataURL: string): Promise<string | null> {
   try {
-    const res = await fetch(`${BACKEND_URL}/job/_test`);
-    return URL.createObjectURL(await res.blob());
+    // Create job
+    const jobResponse = await fetch(`${BACKEND_URL}/job/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ prompt: prompt, image: { image_data: dataURL } })
+    });
+    const jobData = await jobResponse.json();
+    const jobId = jobData.job_id;
+
+    // Check job status
+    let jobStatus = 'NOT_STARTED';
+    while (jobStatus !== 'COMPLETE' && jobStatus !== 'FAILED') {
+      await new Promise(r => setTimeout(r, 1000));  // Wait for a second
+      const statusResponse = await fetch(`${BACKEND_URL}/job/?job_id=${jobId}`);
+      const statusData = await statusResponse.json();
+      jobStatus = statusData.status;
+    }
+
+    // If job failed, return null
+    if (jobStatus === 'FAILED') {
+      console.error(`Job failed: ${jobId}`);
+      return null;
+    }
+
+    // Get job result
+    const resultResponse = await fetch(`${BACKEND_URL}/job/${jobId}`);
+    return URL.createObjectURL(await resultResponse.blob());
   } catch (err) {
     console.error(`Error generating QR code: ${err}`);
+    return null;
   }
-  return null;
 }
