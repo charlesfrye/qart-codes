1f23fcdca773e772e2c79377887d6a2408cdbe04
Charles Frye <cfrye59@gmail.com>
2023-06-12 01:47:34 -0700

    adds more loading phrases

--- src/config.ts
+++ src/config.ts
@@ -12,6 +12,24 @@ export const LOADING_MESSAGES = [
   `Denoising latents...`,
   `Competing for attention...`,
   `Literally just vibing...`,
+  `Making ES6 Promises we can't keep...`,
+  `Mining XMR in your browser...`,
+  `Fiddling with hyperparameters...`,
+  `Deserializing data so we can serialize it again...`,
+  `Looking up which way the KL Divergence goes...`,
+  `Logging your personal data to Grafana "on accident"...`,
+  `Violating the GDPR and the Geneva Convention...`,
+  `Scrolling through the terms of service...`,
+  `Publishing in ICLR...`,
+  `Hugging some faces...`,
+  `Synthesizing aesthetics...`,
+  `Inventing new colors...`,
+  `Getting ChatGPT to rewrite the codebase right quick...`,
+  `Whispering to the bits and bytes...`,
+  `Rolling out with the Transfomers...`,
+  `Taking a GANder...`,
+  `Rolling the art dice...`,
+  `Torching a pie...`,
 ];
 
-export const LOADING_MESSAGE_INTERVAL_MS = 2000;
+export const LOADING_MESSAGE_INTERVAL_MS = 5000;
