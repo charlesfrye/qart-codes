6d0b003b4f29f246cb9dbe631872add13cdb1def
Charles Frye <charles@modal.com>
2025-01-28 23:33:26 -0800

    remove old 'scannability.py'

--- backend/evals/__init__.py
+++ backend/evals/__init__.py
@@ -1,9 +1,8 @@
 import modal
 from .aesthetics import app as aesthetics_app
-from .scannability import app as scannability_app
 from .scannability_qreader import app as qreader_app
 
 app = modal.App(name="qart-eval")
+
 app.include(aesthetics_app)
-app.include(scannability_app)
 app.include(qreader_app)
