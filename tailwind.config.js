a7fa056ef43db498e5dc48575a34ed126fdbdbe8
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2025-05-30 10:53:48 -0500

    Placeholder font for loading messages

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,13 @@ export default {
       fontFamily: {
         degular: ["Degular", "sans-serif", "system-ui"],
         inter: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+        pixel: ['"Press Start 2P"', "monospace"],
       },
       colors: {
         black: `#000000`,
         green: `#84CC16`,
         "green-light": `#DDFFDC`,
+        "green-font": `#7FEE64`,
         gray: "#161916",
       },
     },
