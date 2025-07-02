015c8f9727e728724d94c18e1b0b7726e02bc091
Charles Frye <cfrye59@gmail.com>
2025-07-02 11:54:57 -0400

    add vercel analytics (#51)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App.tsx";
@@ -8,6 +9,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Analytics />
     <ToastContainer />
   </React.StrictMode>
 );
